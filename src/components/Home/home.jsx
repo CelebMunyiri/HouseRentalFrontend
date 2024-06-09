ea634d6a3f240febf25cb66ba1cfe648856b6a8a
David Munyiri <davidmumyiri2019@gmail.com>
2024-06-09 15:44:21 +0300

    made a change

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,10 +1,26 @@
+import { useState,useEffect } from "react";
 
 
 function Home(){
+    const [color,setColor]=useState('initial');
+    const [click,setClicks]= useState(0);
+
+    const handleClicks=()=>{
+        setClicks(){
+            click++;
+        }
+    }
+
+    const handleClick=()=>{
+        setColor('red');
+    }
     return(
         <>
         <div>
             <h6>Welcome Home</h6>
+            <button onClick={handleClick} style={{backgroundColor: color==='initial' ?'': color}} >Click Me</button>
+
+            <h4>{click}</h4>
         </div>
         </>
     )
